e0d671834550eb55f682a8edab508b8ecd1b4008
mediamakermatt <mediamakermatt@gmail.com>
2020-04-13 13:28:55 -0400

    debugging

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const path = require('path');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-
 // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + './dist/angular-app'));
+app.use(express.static(__dirname + './dist/angular-app'));
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('/*', function(req,res){   
+//     res.sendFile(path.join(__dirname+'./dist/angular-app/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
